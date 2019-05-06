c306e10b66d072f8b90d545fa7e375df0daeed16
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-05-06 00:27:39 -0500

    Added relavent copy

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -21,7 +21,7 @@ class homepage extends Component {
     <img src="https://images.unsplash.com/photo-1469571486292-0ba58a3f068b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80" alt="educational symbols"  className="content-image"></img>
            
     </div>
-    <p className="mid-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur lorem. In finibus feugiat ante tincidunt porttitor. In hac habitasse platea dictumst. Nulla facilisi. Integer viverra efficitur orci, nec molestie leo. Phasellus efficitur ornare varius. Maecenas faucibus massa sed eros dapibus commodo. Vestibulum ac felis ornare nibh tempor varius eu id leo. Donec eu accumsan enim, a bibendum leo.</p>
+    <p className="mid-text">Scheduling volunteers or physical donation pickups has never been easier. Just pick a time and date and get back to doing what you love.</p>
 
             
            
@@ -32,7 +32,7 @@ class homepage extends Component {
     <img src="https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80" alt="Three impoverished children" className="content-image"></img>
     </div>
          
-          <p className="mid-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in consectetur lorem. In finibus feugiat ante tincidunt porttitor. In hac habitasse platea dictumst. Nulla facilisi. Integer viverra efficitur orci, nec molestie leo. Phasellus efficitur ornare varius. Maecenas faucibus massa sed eros dapibus commodo. Vestibulum ac felis ornare nibh tempor varius eu id leo. Donec eu accumsan enim, a bibendum leo.</p> 
+          <p className="mid-text">Scheduling and recieving donations shouldn't be tough, so let us take care of all the processing. Just add an amount and an image and you're good to go!</p> 
     </div>
     </div>
     
